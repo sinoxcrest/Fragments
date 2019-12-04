362623bad3dcafa30e2f644071324c0f328b11c3
sinoxcrest <sinoxcrest@gmail.com>
2019-12-04 10:48:45 +0545

    ok

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.fragments.fragment.FirstFragment;
+import com.example.fragments.fragment.SecondFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnFragment;
+    private Boolean status=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnFragment=findViewById(R.id.btnfragment);
+        btnFragment.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+    if(status){
+        FirstFragment firstFragment=new FirstFragment();
+        fragmentTransaction.replace(R.id.fragmentContainer,firstFragment);
+        fragmentTransaction.commit();
+        btnFragment.setText("Load Second Fragment");
+        status=false;
+    }else{
+        SecondFragment secondFragment=new SecondFragment();
+        fragmentTransaction.replace(R.id.fragmentContainer,secondFragment);
+        fragmentTransaction.commit();
+        btnFragment.setText("Load First Fragment");
+        status=true;
+    }
+    
+
     }
 }
